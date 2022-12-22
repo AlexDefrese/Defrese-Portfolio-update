7bc4a8a9deca3e68777c2e5371913e6e3a4b5098
Alex Defrese <aidandefrese@gmail.com>
2022-12-21 19:53:49 -0500

    fixed broken link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#001427] text-[#708D81]">
       <div className=' hover:shadow-lg hover:shadow-[#B3C9D0]'>
-        <a href="/">
+        <a href="">
         <img src={Logo} alt='Logo' style= {{width: '75px'}} />
         </a>
       </div>
